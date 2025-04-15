ae1bf04bf5f5f0ff5631118fa9711e95d53aa87b
filog265 <filippo.gorini.p@gmail.com>
2025-04-15 10:18:12 +0200

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,9 +1,77 @@
+import random
 import socket
 host = "vlbelintrocrypto.hevs.ch"  # Le nom du serveur
 port = 6000  # Le numéro de port du serveur
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
 print(host, port)
+
+def isPrime(n, k=5):
+    if n < 2:
+        return False
+    for _ in range(k):
+        a = random.randint(2, n - 2)
+        if pow(a, n-1, n) != 1:
+            return False
+    return True
+
+def generatePrime(bits=512):
+    while True:
+        candidate = random.getrandbits(bits)
+        candidate |= 1  # Force impair
+        candidate |= (1 << (bits - 1))  # Force bit de poids fort
+        if isPrime(candidate):
+            return candidate
+
+def generateGenerator(p):
+    # Essaie de petits nombres jusqu'à trouver un générateur probable
+    for g in range(2, p):
+        if pow(g, 2, p) != 1 and pow(g, (p - 1) // 2, p) != 1:
+            return g
+    raise ValueError("Aucun générateur trouvé")
+
+def privAndPubKey(g, p):
+    privateKey = random.randint(2, p - 2)
+
+    # Étape 2 : calculer clé publique
+    publicKey = pow(g, privateKey, p)
+    return privateKey, publicKey
+
+def secretKey(othKey, privKey, p):
+    sharedSecret = pow(othKey, privKey, p)
+
+    return sharedSecret
+
+def diffieHellmanExchange(g, p):
+    # Étape 1 : choisir une clé privée (aléatoire)
+    privateKey = random.randint(2, p - 2)
+
+    # Étape 2 : calculer clé publique
+    publicKey = pow(g, privateKey, p)
+
+    # Étape 3 : envoyer clé publique au serveur
+    pubKeyBytes = publicKey.to_bytes((publicKey.bit_length() + 7) // 8, byteorder='big')
+    pubKeySize = len(pubKeyBytes)
+
+    message = addMessageHeader("s") + addMessageSize(pubKeySize) + pubKeyBytes
+    s.sendall(message)
+
+    # Étape 4 : recevoir la clé publique de l'autre personne
+    # Lire le header et taille
+    """header = s.recv(6)
+    if not header.startswith(b"ISCs"):
+        raise ValueError("Réponse du serveur invalide")
+
+    size = int.from_bytes(header[4:6], byteorder='big')
+    otherPubKeyBytes = s.recv(size)
+
+    otherPublicKey = int.from_bytes(otherPubKeyBytes, byteorder='big')"""
+    otherPublicKey = reception(b"s")
+
+    # Étape 5 : calcul de la clé secrète partagée
+    sharedSecret = pow(otherPublicKey, privateKey, p)
+
+    return sharedSecret
 
 def lastWord(t):  # Fonction qui retourne le dernier mot d'un str
     return t.rpartition(" ")[2]
@@ -36,39 +104,44 @@ def shiftEncode(msg, key):
         charInt = int.from_bytes(x.encode("utf-8"), byteorder="big")
         charInt += shift
         charByte = charInt.to_bytes(4, byteorder="big")
-        #charUtf8 = charByte.decode("utf-8")
         encodedMsg += charByte
 
     return encodedMsg
 
 def shiftDecode(encodedMsg, key):  # Fonction qui decode un message encodé avec shift
-    decodedMsg = ""
-    for x in encodedMsg:
-        y = ord(x) - int(key)
-        decodedMsg += chr(y).encode("utf-8")
-    return decodedMsg
-
-def vigenereEncode(msg, key):  # Fonction qui encode un message avec l'encodage vigenere
-    position = 0
-    newKey = ""
+    decodedMsg = b""
+    shift = int(key)
 
     for x in msg:
-        newKey += key[position]
-        if position == len(key):
-            position = 0
-        else:
-            position += 1
+        charInt = int.from_bytes(x.encode("utf-8"), byteorder="big")
+        charInt -= shift
+        charByte = charInt.to_bytes(4, byteorder="big")
+        decodedMsg += charByte
 
-    encryptedText = []
+    return decodedMsg
 
-    for i in range(len(msg)):
-        char = ord(msg[i])
-        keyCode = ord(newKey[i])
-        encryptedChar = chr(char + keyCode % 0xffff).encode("utf-8")
-        encryptedText.append(encryptedChar)
-    return "".join(encryptedText)
+def vigenereEncode(msg, key):
+    encodedMsg = b""
+    keyLength = len(key)
+
+    for i, char in enumerate(msg):
+        m = int.from_bytes(char.encode("utf-8"), byteorder="big")
+        k = int.from_bytes(key[i % keyLength].encode("utf-8"), byteorder="big")
+        c = m + k
+        encodedMsg += c.to_bytes(4, byteorder="big")
+
+    return encodedMsg
 
 #def vigenereDecode(encodedMsg, key):  # Fonction qui decode un message encodé avec vigenere
+
+def rsaEncode(msg, n, e):
+    encodedMsg = b""
+    for char in msg:
+        m = int.from_bytes(char.encode("utf-8"), byteorder="big")
+        c = pow(m, e, n)
+        encodedMsg += c.to_bytes(4, byteorder="big")
+    return encodedMsg
+
 
 def sTypeMessage(mT):  # Fonction qui s'occupe des messages de type 's'
     task = input("Enter task: ")
@@ -87,29 +160,105 @@ def sTypeMessage(mT):  # Fonction qui s'occupe des messages de type 's'
         else:
             f = False
 
-    print(taskMessage)
-    taskKey = taskMessage.split("-key ")
-
-    messageToEncodeDecode = reception(b"s")
-
-    print("Message: " + messageToEncodeDecode)
-
     if "shift" in task:
         if "encode" in task:
-            encodedResult = shiftEncode(messageToEncodeDecode, taskKey[1])
+            print(taskMessage)
+            taskKey = taskMessage.split("-key ")[1]
+
+            messageToEncodeDecode = reception(b"s")
+            print("Message: " + messageToEncodeDecode)
+
+            encodedResult = shiftEncode(messageToEncodeDecode, taskKey)
             size = len(encodedResult) // 4
             mess = addMessageHeader(mT) + addMessageSize(size) + encodedResult
-            #print(len(encodedResult))
-            #print(encodedResult)
             s.sendall(mess)
+            print(reception(b"s"))
+
         elif "decode" in task:
-            decodedResult = shiftDecode(messageToEncodeDecode.decode(), taskKey[1])
-            print("Your decoding gave this result: " + decodedResult)
-            s.sendall(addMessageHeader(mT) + addMessageSize(decodedResult) + convertMessage(decodedResult))
+            print(taskMessage)
+            #taskKey = taskMessage.split("-key ")
 
-    #result = reception2(b"s")
+            #messageToEncodeDecode = reception(b"s")
+            messageToEncodeDecode = s.recv(1024)
+            print(b"Message: " + messageToEncodeDecode)
 
-    print(reception(b"s"))
+            """decodedResult = shiftDecode(messageToEncodeDecode, taskKey[1])
+            size = len(decodedResult) // 4
+            mess = addMessageHeader(mT) + addMessageSize(size) + decodedResult
+            s.sendall(mess)"""
+            print(reception(b"s"))
+
+    elif "vigenere" in task:
+        if "encode" in task:
+            print(taskMessage)
+            taskKey = taskMessage.split("-key ")[1]
+
+            messageToEncodeDecode = reception(b"s")
+            print("Message: " + messageToEncodeDecode)
+
+            encodedResult = vigenereEncode(messageToEncodeDecode, taskKey)
+            size = len(encodedResult) // 4
+            mess = addMessageHeader(mT) + addMessageSize(size) + encodedResult
+            s.sendall(mess)
+            print(reception(b"s"))
+
+        elif "decode" in task:
+            print(reception(b"s"))
+            print(reception(b"s"))
+
+    elif "rsa" in task.lower():
+        if "encode" in task:
+            print(taskMessage)
+            nKey = int(taskMessage.split("=")[1][:-3])
+            eKey = int(taskMessage.split("=")[2])
+
+            messageToEncodeDecode = reception(b"s")
+            print("Message: " + messageToEncodeDecode)
+
+            encodedResult = rsaEncode(messageToEncodeDecode, nKey, eKey)
+            size = len(encodedResult) // 4
+            mess = addMessageHeader(mT) + addMessageSize(size) + encodedResult
+            s.sendall(mess)
+            print(reception(b"s"))
+
+        elif "decode" in task:
+            print(reception(b"s"))
+            print(reception(b"s"))
+
+    elif "DifHel" in task:
+        print(taskMessage)
+        prime = generatePrime(10)
+        generator = generateGenerator(prime)
+        primeAndGen = str(prime) + ", " + str(generator)
+
+        print("prime: " + str(prime) + ", generator: " + str(generator))
+        mess = addMessageHeader(mT) + addMessageSize(len(primeAndGen)) + convertMessage(primeAndGen)
+        s.sendall(mess)
+        print(reception(b"s"))
+        otherPubKey = int(reception(b"s"))
+        print(otherPubKey)
+
+        privateKey, publicKey = privAndPubKey(generator, prime)
+        mess2 = addMessageHeader(mT) + addMessageSize(len(str(publicKey))) + convertMessage(str(publicKey))
+        s.sendall(mess2)
+        print(reception(b"s"))
+
+        sharedKey = secretKey(otherPubKey, privateKey, prime)
+        mess3 = addMessageHeader(mT) + addMessageSize(len(str(sharedKey))) + convertMessage(str(sharedKey))
+        s.sendall(mess3)
+        print("shared key: " + str(sharedKey))
+        print(reception(b"s"))
+
+    elif "hash" in task:
+        if "verify" in task:
+            print(reception(b"s"))
+            print(reception(b"s"))
+            print(reception(b"s"))
+
+        else:
+            print(reception(b"s"))
+            print(reception(b"s"))
+
 
 def tTypeMessage(mT):  # Fonction qui s'occupe des messages de type 't'
     message = input("Enter message: ")
@@ -147,7 +296,7 @@ def addMessageHeader(msgType):  # Renvoie le header du message
 
     return header
 
-def addMessageSize(n):  # Renvoie la taille du message
+def addMessageSize(n):  # Renvoie la taille du message dans le bon format
     size = b""
     size += n.to_bytes(2, byteorder="big")
 
